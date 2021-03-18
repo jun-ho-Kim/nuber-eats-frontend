f3a03b1674d9fe3aa13faeed3941261ae2a73743
unknown <wnsgh5049@naver.com>
2021-03-18 10:51:12 +0900

    React form: useState

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { isLoggedInVar } from "../apollo";
 
 export const LoggedOutRouter = () => {
     const onClick = () => {
         isLoggedInVar(true);
     }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const onSubmit = (event: any) => {
+        console.log("submit resule: ", event)
+    } 
+
+    const onChange = (event: any) => {
+        const {target: 
+            {name, value} 
+        } = event;
+        if(name === "email") {
+            setEmail(value)
+        }
+        if(name === "password") {
+            setPassword(value)
+        }
+    }
     return (
         <div>
             <h1>Logged out</h1>
-            <button onClick={onClick}>Click on Login</button>
+            <form onSubmit={onSubmit}>
+            <div>
+                <input 
+                    onChange={onChange}
+                    name="email"
+                    type="email"
+                    required
+                    placeholder="email"
+                    value={email}
+                />
+                <input
+                    onChange={onChange}
+                    name="password"
+                    type="password"
+                    required
+                    placeholder="password"
+                    value={password}
+                />
+                <button className="bg-yellow-300 text-white">Submit</button>
+            </div>
+            </form>
         </div>
     )
 }

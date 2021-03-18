ab97b497677d9d953e68e481cd1a731024d9f1c3
unknown <wnsgh5049@naver.com>
2021-03-18 09:04:32 +0900

    Local Only Field: gql + useQuery VS Reactive variables + useReactiveVar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { gql, useQuery, useReactiveVar } from '@apollo/client';
 import React from 'react';
+import { isLoggedInVar } from './apollo';
+import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
 
+// const IS_LOGGED_IN = gql`
+//   query isLoggedIn {
+//     isLoggedIn @client
+//   }
+// `
+
 function App() {
-  return <LoggedOutRouter />;
+  // const {data: {isLoggedIn}} = useQuery(IS_LOGGED_IN);
+  const isLoggedIn = useReactiveVar(isLoggedInVar)
+  return isLoggedIn? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,14 @@
 import React from "react";
+import { isLoggedInVar } from "../apollo";
 
-export const LoggedOutRouter = () => <span>Logged</span>;
+export const LoggedOutRouter = () => {
+    const onClick = () => {
+        isLoggedInVar(true);
+    }
+    return (
+        <div>
+            <h1>Logged out</h1>
+            <button onClick={onClick}>Click on Login</button>
+        </div>
+    )
+}

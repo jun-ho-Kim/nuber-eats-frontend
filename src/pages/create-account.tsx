b60efa648aea060d48a9f260e3ceb509e7eee679
unknown <wnsgh5049@naver.com>
2021-03-24 11:43:35 +0900

    LoginForm with tailwindCSS + formState

--- src/pages/create-account.tsx
+++ src/pages/create-account.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const CreateAccount = () => {
-    return <span>Create Account</span>
-};
+    return (
+        <div>
+            <span>Create Account</span>
+        </div>
+    )
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,12 @@
 import { gql, useMutation } from "@apollo/client";
 import React from "react";
+import nuberLogo from "../images/logo.svg"
 import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
 import { loginMutation, loginMutationVariables, loginMutation_login } from "../__generated__/loginMutation";
+import { Button } from "../components/button";
+import { Link } from "react-router-dom";
+
 
 const LOGIN_MUTATION = gql`
     mutation loginMutation($loginInput: LoginInput){
@@ -31,7 +35,9 @@ const onCompleted = (data: loginMutation) => {
 const onError = () => {};
 
 export const Login = () => {
-    const {register, getValues, watch, errors, handleSubmit} = useForm<ILoginForm>()
+    const {register, getValues, watch, errors, handleSubmit, formState} = useForm<ILoginForm>({
+        mode: "onChange"
+    })
     const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
         loginMutation, 
         loginMutationVariables
@@ -52,12 +58,15 @@ export const Login = () => {
     }
 
     return (
-        <div className="h-screen flex items-center justify-center bg-gray-800">
-            <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
-                <h3 className="text-2xl text-gray-800">Login</h3>
+        <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+            <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+                <img src={nuberLogo} className="w-52 mb-10" />
+                <h4 className="w-full font-medium text-left text-3xl mb-5">
+                    Welcome back
+                </h4>
                 <form
                     onSubmit={handleSubmit(onSubmit)}
-                    className="grid gap-3 mt-5 px-5"
+                    className="grid gap-3 mt-5 w-full mb-5"
                 >
                     <input
                         ref={register({ required: "Email is required "})}
@@ -84,13 +93,22 @@ export const Login = () => {
                     {errors.password?.type === "minLength" && (
                         <FormError errorMessage="Password must be more than 10 chars." />
                     )}
-                    <button className="mt-3 btn">
-                        {loading? "Loading..." : "Log In"}
-                    </button>
+                    <Button
+                        canClick={formState.isValid}
+                        loading={loading}
+                        actionText={"Log in"}
+                    />
                     {loginMutationResult?.login.error && (
                         <FormError errorMessage={loginMutationResult.login.error} />
                     )} 
                 </form>
+                <div>
+                    New to Nuber?{" "}
+                    <Link to="/create-account" className="text-emerald-600 hover:underline">
+                        Create Account
+                    </Link>
+
+                </div>
             </div>
         </div>
     )

9c2c6444faa5d6c6ab103aadf966ef091e859e79
unknown <wnsgh5049@naver.com>
2021-03-26 17:12:33 +0900

    Saving and Using Token

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,8 +6,9 @@ import { FormError } from "../components/form-error";
 import { loginMutation, loginMutationVariables } from "../__generated__/loginMutation";
 import { Button } from "../components/button";
 import { Link } from "react-router-dom";
-import Helmet from "react-helmet";
-import { isLoggedInVar } from "../apollo";
+import { Helmet } from "react-helmet-async";
+import { authTokenVar, isLoggedInVar } from "../apollo";
+import { LOCALSTORAGE_TOKEN } from "../constants";
 
 const LOGIN_MUTATION = gql`
     mutation loginMutation($loginInput: LoginInput!){
@@ -35,8 +36,10 @@ export const Login = () => {
             login: { error, ok, token }
         } = data;
         try {
-            if(ok) {
+            if(token) {
                 console.log(token);
+                localStorage.setItem(LOCALSTORAGE_TOKEN, token);
+                authTokenVar(token);
                 isLoggedInVar(true);
             }
         } catch(error) {
@@ -68,7 +71,7 @@ export const Login = () => {
                 <title>login | Nuber Eeats</title>
             </Helmet>
             <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
-                <img src={nuberLogo} className="w-52 mb-10" />
+                <img src={nuberLogo} className="w-52 mb-10" alt="Nuber Eats" />
                 <h4 className="w-full font-medium text-left text-3xl mb-5">
                     Welcome back
                 </h4>
@@ -79,7 +82,7 @@ export const Login = () => {
                     <input
                         ref={register({ 
                             required: "Email is required",
-                            pattern : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                            pattern : /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                         })}
                         name="email"
                         required
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,7 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { isLoggedInVar } from "../apollo";
+// import { meQuery } from "../__generated__/meQuery";
+import { meQuery } from "../__generated__/meQuery";
+
+const ME_QUERY = gql`
+    query meQuery {
+        me {
+          id
+          email
+          role
+          verified
+        }
+    }
+`;
 
 export const LoggedInRouter = () => {
+    const {data, loading, error} = useQuery<meQuery>(ME_QUERY);
+    console.log("data:", data);
+    console.log("data:", data);
+    if(!data || loading || error) {
+        return (
+            <div className="h-screen flex justify-center items-center">
+                <span className="font-medium text-xl tracking-wide">
+                    Loaidng...
+                </span>
+            </div>
+        )
+    }
     const onClick = () => {
         isLoggedInVar(false)
     }
@@ -12,8 +38,5 @@ export const LoggedInRouter = () => {
             <button onClick={onClick}>Log Out</button>
         </div>
     )
-
-
-
 } 
     
